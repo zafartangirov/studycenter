85d1851fb50d46b0b07068a3e692110c925ba679
Zafar <a.b@gmail.com>
2024-12-05 15:07:19 +0500

    PaymentRepo commit

--- src/main/java/uz/pdp/demo9/studycenter/repo/PaymentRepo.java
+++ src/main/java/uz/pdp/demo9/studycenter/repo/PaymentRepo.java
@@ -1,13 +1,15 @@
 package uz.pdp.demo9.studycenter.repo;
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.Query;
 import uz.pdp.demo9.studycenter.Payment;
+import uz.pdp.demo9.studycenter.Student;
 
 import java.util.List;
 
 import static uz.pdp.demo9.MyListener.EMF;
 
-public class PaymentRepo {
+public class PaymentRepo extends BaseRepo<Payment>{
     public static List<Payment> getAllPayments() {
         try (
                 EntityManager entityManager = EMF.createEntityManager()
@@ -17,18 +19,39 @@ public class PaymentRepo {
             throw new RuntimeException(e);
         }
     }
-    public static List<Payment> getStudentsAndPayments(int groupId) {
-        try (EntityManager entityManager = EMF.createEntityManager()) {
-            String query = """
-                    select s.fullname, p.*, sum(distinct p.amount)
-                            from payment p
-                                     join student s on p.student_id = s.id
-                            where s.groups_id = %d
-                            group by s.fullname, p.amount, p.id, p.student_id
-            """.formatted(groupId);
-            return entityManager.createNativeQuery(query,Payment.class).getResultList();
+
+    public static List<Payment> getStudentsAndPayments(int id) {
+        try (
+                EntityManager entityManager = EMF.createEntityManager()
+        ) {
+           return entityManager.createNativeQuery("select p.* from payment p join student s on p.student_id = s.id where s.groups_id = %d".formatted(id), Payment.class).getResultList();
         } catch (Exception e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    public static List<Payment> getPaymentsList(int id, int pagejon, String search) {
+        pagejon--;
+        try (
+                EntityManager entityManager = EMF.createEntityManager()
+        ) {
+            Query selectTFromTodoT = entityManager.createNativeQuery("select p.* from payment p join student s on p.student_id = s.id where s.fullname ilike '%'|| :search ||'%' and s.groups_id = :groupsId limit 10 offset :off", Payment.class)
+                    .setParameter("off", pagejon * 10)
+                    .setParameter("search", search)
+                    .setParameter("groupsId", id);
+
+            return selectTFromTodoT.getResultList();
+        }
+    }
+
+    public static long count(String search, int id) {
+        try (
+                EntityManager entityManager = EMF.createEntityManager()
+        ) {
+            Query selectTFromTodoT = entityManager.createNativeQuery("select count(p.*) from payment p join student s on p.student_id = s.id where s.fullname ilike '%'|| :search ||'%' and s.groups_id = :groupsId", Long.class)
+                    .setParameter("search", search)
+                    .setParameter("groupsId", id);
+            return (Long)selectTFromTodoT.getSingleResult();
         }
     }
 }
