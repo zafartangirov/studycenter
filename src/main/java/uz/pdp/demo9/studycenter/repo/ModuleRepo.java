b78c65e308ad82eb3f08fcb6b4c545c32090c94c
Zafar <a.b@gmail.com>
2024-12-05 15:06:13 +0500

    ModuleRepo commit

--- src/main/java/uz/pdp/demo9/studycenter/repo/ModuleRepo.java
+++ src/main/java/uz/pdp/demo9/studycenter/repo/ModuleRepo.java
@@ -1,13 +1,14 @@
 package uz.pdp.demo9.studycenter.repo;
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.Query;
 import uz.pdp.demo9.studycenter.Modules;
 
 import java.util.List;
 
 import static uz.pdp.demo9.MyListener.EMF;
 
-public class ModuleRepo {
+public class ModuleRepo extends BaseRepo<Modules>{
 
 
     @SuppressWarnings("all")
@@ -33,6 +34,31 @@ public class ModuleRepo {
                     """, Modules.class).getResultList();
         } catch (Exception e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    public static List<Modules> getModulesList(Integer id, int pagejon, String search) {
+        pagejon--;
+        try (
+                EntityManager entityManager = EMF.createEntityManager()
+        ) {
+            Query selectTFromTodoT = entityManager.createNativeQuery("select * from modules where name ilike '%'|| :search ||'%' and course_id = :courseId limit 10 offset :off", Modules.class)
+                    .setParameter("off", pagejon * 10)
+                    .setParameter("search", search)
+                    .setParameter("courseId", id);
+
+            return selectTFromTodoT.getResultList();
+        }
+    }
+
+    public static long count(String search, Integer id) {
+        try (
+                EntityManager entityManager = EMF.createEntityManager()
+        ) {
+            Query selectTFromTodoT = entityManager.createNativeQuery("select count(*) from modules where name ilike '%'|| :search ||'%' and course_id = :courseId", Long.class)
+                    .setParameter("search", search)
+                    .setParameter("courseId", id);
+            return (Long)selectTFromTodoT.getSingleResult();
         }
     }
 }
